e829d87a05625e22ec47b14f67884e2407fe8c01
axmetmercan <axmetmercan@outlook.com>
2022-01-22 16:27:04 +0300

    .

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,7 @@
 
 from unicodedata import category
 from django.shortcuts import redirect, render, HttpResponse, get_object_or_404,reverse
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from portfolio.models import AboutMe, Categories, Hobbie, Project, Resume, Study, WorkExperience, myportfolio
 
 # Create your views here.
@@ -45,9 +45,20 @@ def my_projects(request):
 
     projects = Project.objects.all().order_by('-public_date')
     categories = Categories.objects.all()
+    page = request.GET.get("page",1)
+    paginator = Paginator(projects, 1)
+
+    try:
+        all_projects = paginator.page(page)
+    except PageNotAnInteger:
+        all_projects = paginator.page(1)
+    except EmptyPage:
+        all_projects = paginator.page(paginator.num_pages)
+
+
 
     context = {
-        "projects" : projects,
+        "projects" : all_projects,
         "categories":categories,
     }
     return render(request, 'myprojects.html', context)
@@ -58,9 +69,21 @@ def my_projects_with_cat(request, arg):
     catcegory = Categories.objects.filter(name = arg.replace("-", " ").lower().title())
     projects = Project.objects.filter(category = catcegory[0])
 
+    page = request.GET.get("page",1)
+    paginator = Paginator(projects, 1)
+
+    try:
+        all_projects = paginator.page(page)
+    except PageNotAnInteger:
+        all_projects = paginator.page(1)
+    except EmptyPage:
+        all_projects = paginator.page(paginator.num_pages)
+
+
+
     context = {
         "category": catcegory[0],
-        "projects":projects,
+        "projects":all_projects,
     }
    
     
